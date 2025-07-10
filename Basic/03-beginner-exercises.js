30874caa2bd4441fd802a43db67f930913a806b8
WilliamBG Code <wylcetal@gmail.com>
2025-07-10 17:08:47 -0500

    Agregar más archivos del curso JavaScript.

---
+++ Basic/03-beginner-exercises.js
@@ -0,0 +1,48 @@
+// 1. Escribe un comentario en una linea
+
+// 2. Escribe un comentario en varias lineas
+
+/* Hola, este es un comentario en varias lineas
+y este es el segundo comentario */
+
+// 3. Declara variables co valores asociados a todos los datos de tipo primitivo
+let myName = "William Barsaya"
+let myAge = 20
+let myIsStudent = true
+let myUndefinedValue
+let myNullValue = null
+let mySymbol = Symbol("mySymbol")
+let myBigInt = BigInt(123456789012345678901234567890)
+
+// 4. Imprime el valor de cada variable
+console.log(myName)
+console.log(myAge)
+console.log(myIsStudent)
+console.log(myUndefinedValue)
+console.log(myNullValue)
+console.log(mySymbol)
+console.log(myBigInt)
+
+// 5. Imprime el tipo de cada variable
+console.log(typeof myName)
+console.log(typeof myAge)
+console.log(typeof myIsStudent)
+console.log(typeof myUndefinedValue)
+console.log(typeof myNullValue)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+
+// 6. Declara constantes con valores asociados a todos los datos de tipo primitivo
+const myApellido = "Barsaya"
+const myHeight = 1.75
+const myIsEmployee = false
+const mySymbol2 = Symbol("mySymbol2")
+const myBigInt2 = BigInt(123456789012345678901234567890)
+const myNullValue2 = null
+
+console.log(myApellido)
+console.log(myHeight)
+console.log(myIsEmployee)
+console.log(mySymbol2)
+console.log(myBigInt2)
+console.log(myNullValue2)
