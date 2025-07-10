4e7f835b51864693f55fca9143d357c009771487
WilliamBG Code <wylcetal@gmail.com>
2025-07-10 17:18:51 -0500

    Subir archivos de ejercicios de módulos

---
+++ Basic/31-module-exercices.js
@@ -0,0 +1,7 @@
+// 31-module-exercices.js
+// Ejercicios de módulos
+
+// Ejercicio 1
+import saludar, { sumar } from "./28-export-modules.js";
+console.log(sumar(5, 7));
+console.log(saludar("Copilot"));
---
+++ Basic/32-import-module-exercices.js
@@ -0,0 +1,6 @@
+// 32-import-module-exercices.js
+// Ejercicios de importación de módulos
+
+// Ejercicio 1
+const path = require("path");
+console.log(path.basename(__filename));
